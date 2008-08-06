836535b333f00da1d9a7eedae57469d5fd25dbbd
Leon.van.der.Ree <Leon.van.der.Ree@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-06 08:45:43 -0500

    improved loading indicator
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjsThemePlugin/trunk@10709 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- web/js/Ext.ComponentMgr.create.createInterceptor.js
+++ web/js/Ext.ComponentMgr.create.createInterceptor.js
@@ -1,11 +1,14 @@
 Ext.ComponentMgr.create = Ext.ComponentMgr.create.createInterceptor(function(config, defaultType){
   var xtype = config.xtype || defaultType;
   if (!this.hasType(xtype)){
-    // show a Load Message
-    var loadMessage = Ext.MessageBox.wait("Loading Panel", "Please Wait...");
 
-    Ext.app.CodeLoader.load( {async:false, method:'GET', cacheResponses:true}, '/js/getXtype/' + xtype );
+    Ext.app.CodeLoader.load(
+      Ext.MessageBox.wait("Loading Panel", "Please Wait..."),
+      {async:false, method:'GET', cacheResponses:true},
+      '/js/getXtype/' + xtype
+    );
 
-    loadMessage.hide();
+    Ext.MessageBox.hide()
+
   }
 });
