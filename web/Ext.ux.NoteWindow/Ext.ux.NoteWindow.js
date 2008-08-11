0b108ca8b077cbac0fdc04f56eb987c89237a413
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-11 13:00:07 -0500

    New notewindow extension class to create a nice notes/comments window with running history.
    Example usage at the bottom of Ext.ux.NoteWindow.js.  Example required action methods:
    
      public function executeNoteJsonList()
      {
        $id = $this->getRequestParameter('id');
        $this->forward404Unless($id);
        $c = new Criteria();
        $c->add(LbCommentPeer::LB_REQUEST_ADMIN_ID, $id);
        $c->addDescendingOrderByColumn(LbCommentPeer::CREATED_AT);
        $comments = LbCommentPeer::doSelect($c);
    
        if($comments){
          foreach ($comments as $comment)
          {
            $jsonArr[] = array(
              'comment' => $comment->getContent(),
              'date'    => $comment->getCreatedAt(),
              'user'    => $comment->getsfGuardUser()->__toString(),
              'username'=> $comment->getsfGuardUser()->getProfile()->__toString()
            );
          }
        } else {
          $jsonArr[] = array('comment' => 'No notes found', 'date' => date('Y-m-d h:i:s'));
        }
        $json =  '{"notes":'.json_encode($jsonArr).'}';
    
        return $this->renderText($json);
    
      }
    
      public function executeNoteAjaxUpdate()
      {
        $this->note = $this->getRequestParameter('note');
        $this->id = $this->getRequestParameter('id');
    
        if(!$this->id||!$this->note) $this->forward('lbrequest','ajaxFailed');
    
        $this->addNote();
    
        return $this->renderText('{success:true}');
      }
    
      protected function addNote()
      {
        $comment = new LbComment();
        $comment->setLbRequestAdminId($this->id);
        $comment->setsfGuardUser($this->getUser()->getGuardUser());
        $comment->setContent($this->note);
        $comment->save();
      }
    
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjsThemePlugin/trunk@10787 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ web/Ext.ux.NoteWindow/Ext.ux.NoteWindow.js
@@ -0,0 +1,131 @@
+Ext.ux.NoteWindow = Ext.extend(Ext.Window, {
+  submitNote : function()
+  {
+    this.editFormPanel.form.submit({
+      waitMsg : 'Please Wait...',
+      success : function()
+      {
+        this.hide();
+        this.relatedStore.load();
+      },
+      scope : this
+    });
+  },
+  constructor : function(c)
+  {
+    this.noteStore = new Ext.data.Store({
+      proxy : new Ext.data.HttpProxy({
+        url : c.notesUrl,
+        method : 'POST'
+      }),
+      reader : new Ext.data.JsonReader({
+        root : 'notes'
+      }, [{
+        name : 'user'
+      }, {
+        name : 'date'
+      }, {
+        name : 'comment'
+      }, {
+        name : 'username'
+      }])
+    });
+    this.noteTpl =
+      new Ext.XTemplate('<tpl for=".">' + '<div class="commentblock">' + '<div class="cheader">'
+        + '<div class="cuser" ext:qtip="{username}">{user}:</div>' + '<div class="commentmetadata">{date}</div>'
+        + '</div>' + '<span>{comment}</span>' + '</div>' + '</tpl>');
+
+    this.noteStore.baseParams = {
+      id : c.id
+    };
+    this.noteStore.load();
+
+    this.editFormPanel = new Ext.form.FormPanel({
+      method : 'POST',
+      url : c.notesUpdateUrl,
+      waitMsg : 'Loading Data...',
+      baseParams : {
+        id : c.id
+      },
+      autoHeight : true,
+      bodyStyle : 'padding:10px 10px 5px 10px;',
+      hideBorders : true,
+      items : [{
+        xtype : 'fieldset',
+        autoHeight : true,
+        title : 'Notes',
+        defaultType : 'htmleditor',
+        items : [{
+          hideLabel : true,
+          name : 'note',
+          enableFont : false,
+          enableSourceEdit : false,
+          enableAlignments : false,
+          enableLists : false,
+          width : 364
+        }, {
+          xtype : 'panel',
+          bodyStyle : 'margin-top:5px;margin-bottom:5px;',
+          frame : false,
+          autoScroll : true,
+          height : 280,
+          layout : 'fit',
+          items : new Ext.DataView({
+            autoWidth : true,
+            tpl : this.noteTpl,
+            store : this.noteStore,
+            itemSelector : '.cheader'
+          })
+        }]
+      }]
+    });
+
+    Ext.ux.NoteWindow.superclass.constructor.call(this, Ext.applyIf(c || {}, {
+      title : 'Turnover Notes',
+      buttonAlign : 'center',
+      closable : true,
+      bodyStyle : 'padding-bottom:0px;',
+      width : 420,
+      height : 600,
+      border : false,
+      plain : true,
+      // layout : 'fit',
+      proxyDrag : false,
+      resizable : false,
+      modal : true,
+      closeAction : 'hide',
+      items : this.editFormPanel,
+      buttons : [{
+        text : 'Submit',
+        handler : this.submitNote.createDelegate(this)
+      }]
+    }, c));
+
+    if(Ext.util.CSS.getRule('.cheader')==null)
+    {
+      var styleBody =
+      '.note-l {background: transparent url(/sfExtjsThemePlugin/Ext.ux.NoteWindow/images/comment.gif) no-repeat left;}'
+        + '.note-r {text-align: right;line-height: 16px !important;}'
+        + '.cheader {height: 20px;}'
+        + '.cuser {background: transparent url(/sfExtjsThemePlugin/Ext.ux.NoteWindow/images/comment.gif) no-repeat scroll 0% 0%; clear: none; float: left; font-family: tahoma, arial; font-size: 12px; font-style: normal; font-variant: normal; font-weight: bold; line-height: normal; padding-left: 21px; padding-top: 2px;}'
+        + '.commentmetadata {clear: none; color: gray; display: block; float: right; font-size: 9px; font-weight: normal; margin: 0 0 10px;}'
+        + '.commentblock {background: transparent url(/sfExtjsThemePlugin/Ext.ux.NoteWindow/images/comment-bg.gif) repeat-x scroll 0 -5px; color: #333333; display: block; font-size: 11px; padding: 10px 10px 15px;}';
+
+      var styleSheet = Ext.util.CSS.createStyleSheet('/* Ext.ux.NoteWindow stylesheet */\n'+styleBody, 'NoteWindow');
+      Ext.util.CSS.refreshCache();
+    }
+
+
+
+  }
+});;
+
+Ext.reg('notewindow', Ext.ux.NoteWindow);
+
+/*note = new Ext.ux.NoteWindow({
+  notesUrl: 'lbrequest/noteJsonList',
+  notesUpdateUrl: 'lbrequest/noteAjaxUpdate',
+  relatedStore:  ticketTabs.getComponent(0).store,
+  id: 1
+});
+note.show(document.body);*/
