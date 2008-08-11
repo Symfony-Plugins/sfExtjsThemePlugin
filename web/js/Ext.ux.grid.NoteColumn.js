341760cd271f9c5489194b05d897ed880117b008
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-11 15:13:03 -0500

    Initial start on NoteColumn grid plugin.  Not working yet.
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjsThemePlugin/trunk@10788 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ web/js/Ext.ux.grid.NoteColumn.js
@@ -0,0 +1,44 @@
+Ext.namespace('Ext.ux.grid');
+Ext.ux.grid.NoteColumn = function(config) {
+  Ext.apply(this, config);
+  if (!this.id) {
+    this.id = Ext.id();
+  }
+  this.renderer = this.renderer.createDelegate(this);
+  //this.grid = grid;
+  this.grid.on('render', function()
+  {
+    var view = this.grid.getView();
+    view.mainBody.on('mousedown', this.onMouseDown, this);
+  }, this);
+};
+
+Ext.extend(Ext.ux.grid.NoteColumn, Ext.util.Observable, {
+//  init : function(grid) {
+//    this.grid = grid;
+//    this.grid.on('render', function() {
+//      var view = this.grid.getView();
+//      view.mainBody.on('mousedown', this.onMouseDown, this);
+//    }, this);
+//  },
+
+  onMouseDown : function(e, t) {
+    if (t.className && t.className.indexOf('x-grid3-cc-' + this.id) != -1) {
+      e.stopEvent();
+      var index = this.grid.getView().findRowIndex(t);
+      var note = new Ext.ux.NoteWindow({
+        notesUrl: this.notesUrl,
+        notesUpdateUrl: this.notesUpdateUrl,
+        relatedStore:  this.grid.store,
+        id: index
+      });
+      note.show(document.body);
+    }
+  },
+
+  renderer : function(v, p, record) {
+    return v;
+  }
+});
+
+Ext.reg('notecolumn', Ext.ux.grid.NoteColumn);
