6c4c38fcf34c87cff8bda48e1872eb99199e2f34
Leon.van.der.Ree <Leon.van.der.Ree@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-11 21:16:16 -0500

    added try-catch to prevent errors when loading data while tiny-mce editor already closed
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjsThemePlugin/trunk@10799 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- web/js/Ext.form.BasicForm.override.js
+++ web/js/Ext.form.BasicForm.override.js
@@ -1,4 +1,5 @@
 // allows for preloading when loading forms with combos
+// added catching of errors not throwing errors. (to prevent braking app when closing windows while still setting values form load event (tiny-mce editors))
 
 Ext.form.BasicForm.override({
     /**
@@ -37,7 +38,12 @@ Ext.form.BasicForm.override({
                     if (typeof field.preload == 'function') {
                         field.preload(values[id], values[field.preloadedField]);
                     }
-                    field.setValue(values[id]);
+                    try {
+                        field.setValue(values[id]);
+                    } catch(ex) {
+                        alert(ex);
+                        return; //prevent killing entire app when loading data while tiny-mce editor has already been closed
+                    }
                     if(this.trackResetOnLoad){
                         field.originalValue = field.getValue();
                     }
