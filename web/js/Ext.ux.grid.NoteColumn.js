1e5a58d68efbd26b4f435a1b28902ac9bf10eb86
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-18 14:37:51 -0500

    notecolumn changes.  Need to make more generic
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjsThemePlugin/trunk@10931 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- web/js/Ext.ux.grid.NoteColumn.js
+++ web/js/Ext.ux.grid.NoteColumn.js
@@ -1,37 +1,76 @@
 Ext.namespace('Ext.ux.grid');
-Ext.ux.grid.NoteColumn = function(config) {
+Ext.ux.grid.NoteColumn = function(config)
+{
   Ext.apply(this, config);
-  if (!this.id) {
+  if (!this.id)
+  {
     this.id = Ext.id();
+    this.width = 80;
+    this.sortable = false;
   }
   this.renderer = this.renderer.createDelegate(this);
 };
 
 Ext.extend(Ext.ux.grid.NoteColumn, Ext.util.Observable, {
-  init : function(grid) {
+  init : function(grid)
+  {
     this.grid = grid;
-    this.grid.on('render', function() {
+    this.grid.on('render', function()
+    {
       var view = this.grid.getView();
-      view.mainBody.on('mousedown', this.onMouseDown, this);
+      view.mainBody.on('dblclick', this.onDblClick, this);
     }, this);
   },
 
-  onMouseDown : function(e, t) {
-    if (t.className && t.className.indexOf('x-grid3-cc-' + this.id) != -1) {
+  onDblClick : function(e, t)
+  {
+    if (t.className && t.className.indexOf('x-grid3-nc-' + this.id) != -1)
+    {
       e.stopEvent();
       var index = this.grid.getView().findRowIndex(t);
-      var note = new Ext.ux.NoteWindow({
-        notesUrl: this.notesUrl,
-        notesUpdateUrl: this.notesUpdateUrl,
-        relatedStore:  this.grid.store,
-        id: index
-      });
-      note.show(document.body);
+      var record = this.grid.store.getAt(index);
+      if (!Ext.app.note)
+      {
+        Ext.app.note = new Ext.ux.NoteWindow({
+          notesUrl : this.notesUrl,
+          notesUpdateUrl : this.notesUpdateUrl,
+          relatedStore : this.grid.store,
+          id : record.data.id
+        });
+      }
+      else
+      {
+        Ext.app.note.noteStore.load({
+          params : {
+            id : record.data.id
+          }
+        });
+        Ext.app.note.editFormPanel.form.baseParams = {
+          id : record.data.id
+        };
+      }
+      Ext.app.note.show(t.parentNode.parentNode);
     }
   },
 
-  renderer : function(v, p, record) {
-    return v;
+  renderer : function(v, c, r)
+  {
+    if (Ext.util.CSS.getRule('.note-l') == null)
+    {
+      var styleBody =
+        '.note-l {background: transparent url(/sfExtjsThemePlugin/Ext.ux.NoteWindow/images/comment.gif) no-repeat left;}'
+          + '.note-r {text-align: right;line-height: 16px !important;}';
+
+      var styleSheet =
+        Ext.util.CSS.createStyleSheet('/* Ext.ux.form.NoteColumn stylesheet */\n' + styleBody, 'NoteColumn');
+      Ext.util.CSS.refreshCache();
+    }
+
+    if (!v)
+      return '<p class="x-grid3-note-col x-grid3-nc-' + this.id + '">None</div>';
+    c.css = c.css + "note-r";
+    return String.format('<p ext:qtitle="{0} Wrote:" ext:qtip=\'{1}\' class="note-l  x-grid3-nc-' + this.id
+      + '">{0}</p>', v, r.data['last_comment']);
   }
 });
 
