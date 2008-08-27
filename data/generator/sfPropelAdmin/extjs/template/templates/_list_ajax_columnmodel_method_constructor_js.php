750a33bf33a003905808f77b10566be391e92c78
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-27 09:06:31 -0500

    Added more comments to the variable_cmConfig Cleaned up the code a bit to not pass extra variables from cmConfig to the constructor to then be unset Resolved a bug with invisible fields not incremeting the counter when they were skipped
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjsThemePlugin/trunk@11199 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelAdmin/extjs/template/templates/_list_ajax_columnmodel_method_constructor_js.php
+++ data/generator/sfPropelAdmin/extjs/template/templates/_list_ajax_columnmodel_method_constructor_js.php
@@ -7,25 +7,15 @@
 [?php
 $srcStr = "";
 
-// TODO: WARNING why only do credential checking when the columnmodel has credentials?!?!
 if($columnmodel->plugins)
 {
   foreach($columnmodel->plugins as $key => $value)
   {
-    //I can't think of a better way to do this right now.
-    //We have to determine available credentials during generation but can't compare them until page load
-    //LvanderRee: I think it is OK like this.
-    if(isset($value['editcreds']))
-    {
-      if(!$sf_user->hasCredential($value['editcreds'])) $value['editable']=false;
-      unset($value['editcreds']);
-    }
     $srcStr .= "\nthis.".$key." = Ext.ComponentMgr.create(".$sfExtjs2Plugin->asAnonymousClass($value).");";
   }
 }
 $srcStr .= "
     // columnmodel config
-    //TODO, this should be asAnonymousClass, when config_array really is an array, and not a javascript-object in a string
     this.cmConfig = [".substr($sfExtjs2Plugin->asAnonymousClass($columnmodel->config_array),1,-1)."];
 
     // combine <?php echo $cmName ?>Config with arguments
--- data/generator/sfPropelAdmin/extjs/template/templates/_list_ajax_columnmodel_variable_cmConfig_js.php
+++ data/generator/sfPropelAdmin/extjs/template/templates/_list_ajax_columnmodel_variable_cmConfig_js.php
@@ -22,40 +22,62 @@ $credArr = array();
 $i=0;
 foreach ($columns as $column)
 {
+  //handle credentials for displaying the column
   $listcreds = $this->getParameterValue('list.fields.'.$column->key.'.credentials');
   if ($listcreds)
   {
     $listcreds = str_replace("\n", ' ', var_export($listcreds, true));
+    //if the user doesn't have the right permissions remove the columnconfig
     $credArr[] = 'if(!$sf_user->hasCredential('.$listcreds.')) unset($columnmodel->config_array['.$i.']);';
   }
 
+  //captures the * in the list.display and sets the rowexpander at that position
   if ($column->key == '*')
   {
     $cmItems[] = "{xtype: 'rowexpander'}";
     $i++;
     continue;
   }
-  if (in_array($column->key, $hs) || ($column->isInvisible())) continue;
 
-  if($this->getParameterValue('list.fields.'.$column->key.'.plugin'))
+  //don't create column config for invisible columns
+  if (($column->isInvisible()))
   {
-    //setup the data for generating the new plugin instance
-    $plugins[$column->key.'_'.$this->getParameterValue('list.fields.'.$column->key.'.plugin')] = $this->getColumnAjaxListDefinition($column, $groupedColumns);
-    if ($editcreds = $this->getParameterValue('edit.fields.'.$column->key.'.credentials'))
-    {
-      $editcreds = str_replace("\n", ' ', var_export($editcreds, true));
-      $plugins[$column->key.'_'.$this->getParameterValue('list.fields.'.$column->key.'.plugin')]['editcreds'] = $editcreds;
-    }
-    //set the column item to our generated plugin
-    $cmItems[] = 'this.'.$column->key.'_'.$this->getParameterValue('list.fields.'.$column->key.'.plugin');
     $i++;
     continue;
   }
 
+  //TODO: figure out what we're actually supposed to do with list.hide columns, skipping for now
+  if (in_array($column->key, $hs))
+  {
+    $i++;
+    continue;
+  }
+
+  //handle edit credentials for plugin columns
+  if($this->getParameterValue('list.fields.'.$column->key.'.plugin'))
+  {
+    $pluginArrName = $column->key.'_'.$this->getParameterValue('list.fields.'.$column->key.'.plugin');
+    //setup the data for generating the new plugin instance
+    $plugins[$pluginArrName] = $this->getColumnAjaxListDefinition($column, $groupedColumns);
+    if ($editcreds = $this->getParameterValue('edit.fields.'.$column->key.'.credentials'))
+    {
+      $editcreds = str_replace("\n", ' ', var_export($editcreds, true));
+      //pass our credentials down to the generated partial
+      $credArr[] = "if(!\$sf_user->hasCredential($editcreds)&& is_array(\$columnmodel->plugins['$pluginArrName'])) \$columnmodel->plugins['$pluginArrName']['editable'] = false;";
+    }
+    //set the column item to our generated plugin
+    $cmItems[] = 'this.'.$pluginArrName
+    ;
+    $i++;
+    continue;
+  }
+
+  //handle edit credentials for non-plugin colmns
   if ($editcreds = $this->getParameterValue('edit.fields.'.$column->key.'.credentials'))
   {
     $editcreds = str_replace("\n", ' ', var_export($editcreds, true));
-    $credArr[] = 'if(!$sf_user->hasCredential('.$editcreds.')&& is_array($columnmodel->config_array['.$i.'])&& isset($columnmodel->config_array['.$i.']["editor"])) unset($columnmodel->config_array['.$i.']["editor"]);';
+    //unset the editor if the user doesn't have the right credentials
+    $credArr[] = "if(!\$sf_user->hasCredential($editcreds)&& is_array(\$columnmodel->config_array['$i'])&& isset(\$columnmodel->config_array['$i']['editor'])) unset(\$columnmodel->config_array['$i']['editor']);";
   }
 
   if ($column->isPartial())
