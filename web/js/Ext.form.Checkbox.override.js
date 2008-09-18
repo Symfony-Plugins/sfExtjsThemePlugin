4f56151993c3f30a202a55fd391d3a781ba57213
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-18 12:42:02 -0500

    Added reset event to checkbox
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjsThemePlugin/trunk@11658 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- web/js/Ext.form.Checkbox.override.js
+++ web/js/Ext.form.Checkbox.override.js
@@ -8,5 +8,18 @@ Ext.override(Ext.form.Checkbox, {
       this.resizeEl = Ext.isSafari || Ext.isIE ? this.wrap : (this.wrap.up('.x-form-element', 5) || this.wrap);
     }
     return this.resizeEl;
+  },
+
+  // private
+  initEvents : function()
+  {
+    Ext.form.Checkbox.superclass.initEvents.call(this);
+    this.addEvents('reset');
+    this.initCheckEvents();
+  },
+
+  reset : function()
+  {
+    this.fireEvent('reset', this);
   }
 });
