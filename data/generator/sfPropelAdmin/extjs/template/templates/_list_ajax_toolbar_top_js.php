dbdb705825b80b8f1ca0ca60496dfab124049e1a
KRavEN <KRavEN@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-10-07 07:10:28 -0500

    Altered message on partial messagebox to indicate the generated partial would be in the cache directory
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtjsThemePlugin/trunk@12031 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelAdmin/extjs/template/templates/_list_ajax_toolbar_top_js.php
+++ data/generator/sfPropelAdmin/extjs/template/templates/_list_ajax_toolbar_top_js.php
@@ -68,7 +68,7 @@ $listActions = $this->getParameterValue('list.actions');
   foreach ((array) $listActions as $actionName => $params):
     if($actionName[0] == '_' || isset($params['handler_function'])) continue;
     $this->createPartialFile('_list_ajax_action_'.$actionName,'<?php // @object $sfExtjs2Plugin and @object $toolbar_top provided
-  $configArr["source"] = "Ext.Msg.alert(\'Error\',\'handler_function is not defined!<br><br>Edit the template \"_list_ajax_action_'.$actionName.'.php\" in your application/modules/'.strtolower($this->getModuleName()).'/templates folder and alter it or define the \"handler_function\" in your generator.yml file\');";
+  $configArr["source"] = "Ext.Msg.alert(\'Error\',\'handler_function is not defined!<br><br>Copy the template \"_list_ajax_action_'.$actionName.'.php\" from cache to your application/modules/'.strtolower($this->getModuleName()).'/templates folder and alter it or define the \"handler_function\" in your generator.yml file\');";
   $toolbar_top->attributes["'.$actionName.'"] = $sfExtjs2Plugin->asMethod($configArr);
 ?>');
 ?>
